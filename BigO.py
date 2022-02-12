a940bb0ead5a9ff54fc17e6b02e3572e2bf57371
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-02-12 14:15:04 +0530

    M

---
+++ BigO.py
@@ -0,0 +1,29 @@
+'''
+# O(n)
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+print_items(5)
+print('*' * 50)
+
+
+# O(2n) == O(n)
+def print_items(n):
+    for i in range(n):
+        print(i)
+    for j in range(n):
+        print(j)
+
+print_items(5)
+print('*' * 50)
+'''
+
+# O(n * n) / O(N squared) / O(n2)
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i , j)
+
+print_items(5)
+print('*' * 50)
