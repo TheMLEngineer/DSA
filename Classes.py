3e3abfe510bea87cece3347b742f117450102036
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-02-12 16:25:57 +0530

    M

--- Classes.py
+++ Classes.py
@@ -8,13 +8,18 @@ class Cookie:
         self.color = color
     def get_color(self):
         return self.color
-    def set_color(self):
+    def set_color(self , color):
         self.color = color
 
 
 cookie_one = Cookie('Green')
-print(cookie_one.get_color())
-
 cookie_two = Cookie('Red')
+
+print(cookie_one.get_color())
 print(cookie_two.get_color())
+
+cookie_one.set_color('Wheatish Brown')
+print(cookie_one.get_color())
+print(cookie_two.get_color())
+
 
