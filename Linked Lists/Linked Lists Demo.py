127e4c551fd51760959b55a118422079e43fae3f
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-02-17 23:16:23 +0530

    M

--- Linked Lists/Linked Lists Demo.py
+++ Linked Lists/Linked Lists Demo.py
@@ -14,7 +14,7 @@ class LinkedList:
     def print_list(self):
         temp = self.head
         while temp is not None:
-            print(temp)
+            print(temp.value)
             temp = temp.next
 
     def append(self , value):
@@ -22,15 +22,43 @@ class LinkedList:
         if self.head is None:
             self.head = new_node
             self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while (temp.next):
+            pre = temp
+            temp = temp.next
+            
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+        return temp.value
         
 
-my_linked_list = LinkedList(4)
+my_linked_list = LinkedList(1)
 
-print(my_linked_list.head.value)
+# print(my_linked_list.head.value)
 
+my_linked_list.append(2)
 
+my_linked_list.print_list()
 
-print(my_linked_list.print_list())
-
+print('*' * 50)
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
 
 
