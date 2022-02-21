b8b062414d3ec94048937e26c68ac9ffebf2114d
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-02-21 23:44:59 +0530

    M

--- DoublyLinkedLists.py
+++ DoublyLinkedLists.py
@@ -113,6 +113,22 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
+    def remove(self , index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        temp = self.get(index)
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.next = None
+        temp.prev = None
+        self.length -= 1
+        return temp
+
+
 
             
 
@@ -156,3 +172,6 @@ my_doubly_linked_list.insert(0 , 22)
 my_doubly_linked_list.print_list()
 print('*'* 50)
 
+my_doubly_linked_list.remove(0)
+my_doubly_linked_list.print_list()
+print('*'* 50)
