9c986958644767deafdfb69f5dd6c7630374488e
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-03-16 17:39:28 +0530

    M

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -8,6 +8,50 @@ class BinarySearchTree:
     def __init__(self) -> None:
         self.root = None
 
+    def insert(self , value):
+        new_node = Node(value)
+        # If BST is empty
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        # If BST is not empty
+        while True:
+            # returning true and false in while loop to exit while loop
+            #Using temp pointer
+            # If new value already exist in tree we don't add
+            if new_node.value == temp.value:
+                return False
+            # Ading element in left side
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                # If the point to add value is not in current left side , we are poining to next left side
+                temp = temp.left
+            # Adding element in right side
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                # If the point to add value is not in current right side , we are poining to next right side
+                temp = temp.right
+
+
+print('*' * 50)
+
 my_tree = BinarySearchTree()
 print(my_tree.root) 
+
+print('*' * 50)
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+
+print(my_tree.root.value)
+print(my_tree.root.left.value)
+print(my_tree.root.right.value)
+print('*' * 50)
+
+
 
