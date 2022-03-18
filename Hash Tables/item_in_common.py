f0e7fe6f0a9264cafe640431bf16fc73b2e468be
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-03-18 22:39:49 +0530

    M

---
+++ Hash Tables/item_in_common.py
@@ -0,0 +1,11 @@
+def item_in_common(list1 , list2):
+    my_dict = {}
+    for i in list1:
+        my_dict[i] = True
+    for j in list2:
+        if j in my_dict:
+            return True
+
+    return False
+
+print(item_in_common([1,2,3] , [3,4,5]))
