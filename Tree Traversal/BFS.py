e661d428068b40d759456a89a7ff12081a49719d
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-03-31 03:50:22 +0530

    M

--- Tree Traversal/BFS.py
+++ Tree Traversal/BFS.py
@@ -73,6 +73,29 @@ class BinarySearchTree:
                 queue.append(current_node.right)
         return results
 
+    def dfs_pre_order(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
+    def dfs_post_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+
+        traverse(self.root)
+        return results       
+
 
 
 my_tree = BinarySearchTree()
@@ -84,7 +107,7 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-print(my_tree.BFS())
+print(my_tree.dfs_post_order())
 
     
 
